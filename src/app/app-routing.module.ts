53ac670134054e7ac357d90b706f69eb07e3d0d8
Mario Guadalupe <mariogr93@gmail.com>
2023-05-06 10:56:16 -0400

    add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './components/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "auth",
+    component: AuthenticationComponent
+  },
+  {
+    path: "",
+    redirectTo: "auth",
+    pathMatch: 'full'
+  },
+  {
+    path: "**",
+    redirectTo: "auth",
+  } ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
