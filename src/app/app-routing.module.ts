fd74eec975dc19468d1bb58f97c907be8207071e
Mario Guadalupe <mariogr93@gmail.com>
2023-05-06 22:42:47 -0400

    add notes component to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './components/auth/auth.component';
+import { NotesComponent } from './components/notes/notes.component';
 
 const routes: Routes = [
   {
     path: "auth",
     component: AuthenticationComponent
+  },
+  {
+    path: "auth/notes",
+    component: NotesComponent,
   },
   {
     path: "",
